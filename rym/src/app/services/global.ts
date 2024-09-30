4c1e975a4afa1727de6fc6fefb67e0a8e077c43d
nereagonzalez12 <ereadoce.ng@gmail.com>
2024-09-30 18:08:42 +0200

    urls from ssl backend

--- rym/src/app/services/global.ts
+++ rym/src/app/services/global.ts
@@ -4,6 +4,7 @@
 // export const CHARACTER_URL = 'http://127.0.0.1:8000/api/character/';
 // export const LOCATION_URL = 'http://127.0.0.1:8000/api/location/';
 
+
 /* Production urls*/
 export const DEFAULT_LOCATION_IMG = 'https://wallpapercave.com/wp/wp3960206.jpg';
 export const PAGE_PARAMETER = 'page=';
