22ca8211e8df272fd078968eb724bab06b38b2c1
nereagonzalez12 <ereadoce.ng@gmail.com>
2024-10-01 13:25:36 +0200

    vercel

--- rym/src/app/services/global.ts
+++ rym/src/app/services/global.ts
@@ -12,8 +12,8 @@ export const NAME_PARAMETER = 'name=';
 export const SPECIES_PARAMETER = 'species=';
 export const LOCATION_PARAMETER = 'location_name=';
 
-export const CHARACTER_URL_FILTERS = 'https://16.171.193.109/api/character/?';
-export const LOCATION_URL_FILTERS = 'https://16.171.193.109/api/location/?';
-export const CHARACTER_URL = 'https://16.171.193.109/api/character/';
-export const LOCATION_URL = 'https://16.171.193.109/api/location/';
+export const CHARACTER_URL_FILTERS = 'https://rickypediafront.vercel.app/api/character/?';
+export const LOCATION_URL_FILTERS = 'https://rickypediafront.vercel.app/api/location/?';
+export const CHARACTER_URL = 'https://rickypediafront.vercel.app/api/character/';
+export const LOCATION_URL = 'https://rickypediafront.vercel.app/api/location/';
 
